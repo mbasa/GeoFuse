8acb0ef38e4b4e605abf4b150a215637b9b89701
Mario Basa <mario.basa@gmail.com>
2012-06-25 15:50:14 +0800

    added a description and sample values.

--- src/sql/geoserver_sql_view.sql
+++ src/sql/geoserver_sql_view.sql
@@ -1,5 +1,11 @@
 -- 
+-- Geoserver Parametric SQL for linker1
+--   values: linktab = 'dummy1', maptab = 'mapdummy', mapcol = 'mapcol'
 --
 select a.*,the_geom from %linktab% a,%maptab% b where a.col0 = b.%mapcol%
 
+-- 
+-- Geoserver Parametric SQL for linker2
+--    values: linktab = dummy2;
+--
 select * from %linktab%
