764ded10e36732b522a142ae6acc671f4dc37619
Mario Basa <mario.basa@gmail.com>
2017-10-12 10:21:14 +0900

    create metadata entry only when there are no data errors.

--- src/geotheme/csv/processCSV.java
+++ src/geotheme/csv/processCSV.java
@@ -26,84 +26,85 @@ import geotheme.db.*;
  * Processes the CSV data
  */
 public class processCSV {
-		
 
-	private String db_metadata  = new String();
-	private String db_dynlinker = new String();
-	private String db_csvdata_schema = new String();
-	
+
+    private String db_metadata  = new String();
+    private String db_dynlinker = new String();
+    private String db_csvdata_schema = new String();
+
     public processCSV() {
-        
+
         ResourceBundle rdb = ResourceBundle.getBundle(
-        		"properties.database");
-                
-		this.db_metadata       = rdb.getString("DB.METADATA.TABLE");
-		this.db_dynlinker      = rdb.getString("DB.DYNAMIC_LINKER.TABLE");
-		this.db_csvdata_schema = rdb.getString("DB.CSVDATA_SCHEMA");
-		
+                "properties.database");
+
+        this.db_metadata       = rdb.getString("DB.METADATA.TABLE");
+        this.db_dynlinker      = rdb.getString("DB.DYNAMIC_LINKER.TABLE");
+        this.db_csvdata_schema = rdb.getString("DB.CSVDATA_SCHEMA");
+
     }
 
     public String process(String inputString, String layerName ) {
-		boolean isLatLon = false;
-	      
-		//inputString = (URLDecoder.decode(inputString,"UTF-8"));
+        boolean isLatLon = false;
 
-		String pStr[][] = parseCSV.parse(inputString);
-		
-		if(pStr == null ) {
-			return ("Error: problem with input data");
-		}
-		
-		mapLinkerCtl dlc = new mapLinkerCtl( this.db_dynlinker );
-		Map<String,String[]> dynamicMap = dlc.getMapLinkData();
-		
-		if( !dynamicMap.containsKey(pStr[0][0]) ) {
-			int pStrLen = pStr[0].length;
-			
-			if( pStr[0][pStrLen-1].isEmpty() )
-				pStrLen--;
-			
-			String cLon = pStr[0][pStrLen-2];
-			String cLat = pStr[0][pStrLen-1];
-			
-			if(cLon.equalsIgnoreCase("lon") && 
-               cLat.equalsIgnoreCase("lat") ) {
-				
-				isLatLon = true;
-				
-			}
-			else {
-				return ("Error: 1st column does not match any Mapped column");
-			}
-		}
-				
-		metaDataCtl mdc = new metaDataCtl( this.db_metadata );
-		
-		Random rand = new Random();
-		
-		String tableName = this.db_csvdata_schema + 
-		        ".mb_"+(new Date()).getTime()+ "_" + 
-				rand.nextInt(10000);
-		
-		if( !mdc.setMetaInfo(pStr[0], dynamicMap, this.db_metadata, 
-		        tableName,isLatLon,layerName) ) {
-			return("Error: Catalog entry");
-		}
-		
-		if( isLatLon ) {
-		    String rs = mdc.processDataLatLon(pStr, tableName); 
-			if( rs != null ) {
-				return("Error: Data entry "+rs);
-			}
-		}
-		else {
-		    String rs = mdc.processData(pStr, tableName);
-			if( rs != null ) {
-				return("Error: Data entry "+rs);
-			}
-		}
+        //inputString = (URLDecoder.decode(inputString,"UTF-8"));
 
-		return( tableName );
-	}
+        String pStr[][] = parseCSV.parse(inputString);
+
+        if(pStr == null ) {
+            return ("Error: problem with input data");
+        }
+
+        mapLinkerCtl dlc = new mapLinkerCtl( this.db_dynlinker );
+        Map<String,String[]> dynamicMap = dlc.getMapLinkData();
+
+        if( !dynamicMap.containsKey(pStr[0][0]) ) {
+            int pStrLen = pStr[0].length;
+
+            if( pStr[0][pStrLen-1].isEmpty() )
+                pStrLen--;
+
+            String cLon = pStr[0][pStrLen-2];
+            String cLat = pStr[0][pStrLen-1];
+
+            if(cLon.equalsIgnoreCase("lon") && 
+                    cLat.equalsIgnoreCase("lat") ) {
+
+                isLatLon = true;
+
+            }
+            else {
+                return ("Error: 1st column does not match any Mapped column");
+            }
+        }
+
+        metaDataCtl mdc = new metaDataCtl( this.db_metadata );
+
+        Random rand = new Random();
+
+        String tableName = this.db_csvdata_schema + 
+                ".mb_"+(new Date()).getTime()+ "_" + 
+                rand.nextInt(10000);
+
+        if( isLatLon ) {
+            String rs = mdc.processDataLatLon(pStr, tableName); 
+            if( rs != null ) {
+                return("Error: Data entry "+rs);
+            }
+        }
+        else {
+            String rs = mdc.processData(pStr, tableName);
+            if( rs != null ) {
+                return("Error: Data entry "+rs);
+            }
+        }
+
+        if( !mdc.setMetaInfo(pStr[0], dynamicMap, this.db_metadata, 
+                tableName,isLatLon,layerName) ) {
+            return("Error: Catalog entry");
+        }
+
+
+        return( tableName );
+    }
 
 }
